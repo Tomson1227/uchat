b571e89749a9fc16cff994bea9e3574d8688841c
Oleksandr Povshenko <opovshenko@e1r11p8.unit.ua>
2021-06-09 19:26:49 +0300

    Added start program structure

---
+++ inc/uchat.h
@@ -0,0 +1,17 @@
+#ifndef UCHAT_H
+#define UCHAT_H
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+#include "libmx.h"
+
+#endif /* UCHAT_H */
---
+++ src/main/main.c
@@ -0,0 +1,7 @@
+#include "uchat.h"
+
+int main(int argc, char **argv, char **env)
+{
+    
+    return EXIT_SUCCESS;
+}
