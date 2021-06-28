58f1f5d83f59d28d6eccb368647890ce2577aa38
Pavlo Chernushenko <pavlo.chernushenko@ringteam.com>
2021-06-28 23:57:28 +0300

    Client-Server application demos

---
+++ client/test_client.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+int main (void) {
+
+    //create a socket
+    int network_socket;
+    network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    //specifu an adress for socket
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+    if (connection_status == -1) {
+        printf("Connection failed\n");
+    }
+
+    char server_response[256];
+    recv(network_socket, &server_response, sizeof(server_response), 0);
+
+    //print out response
+    printf("Server send data: %s\n", server_response);
+
+    //close socket
+    close(network_socket);
+
+    return 0;
+}
--- libmx/src/mx_isalpha.c
+++
@@ -1,6 +0,0 @@
-#include "libmx.h"
-
-bool mx_isalpha(char c)
-{
-    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
-}
