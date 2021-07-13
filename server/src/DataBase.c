713c2ec2dbf91ac38ea210d55953630acb00831f
Oleksandr Povshenko <scela2472@gmail.com>
2021-07-13 19:33:47 +0300

    deveop DB table structure

--- server/src/DataBase.c
+++ server/src/DataBase.c
@@ -12,22 +12,27 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
     return 0;
 }
 
-int Init_DB(char *name, char *structure){
+int Init_DB(char *name, const char *structure){
     sqlite3 *db;
     char *zErrMsg = 0;
-    int rc;
     
-    rc = sqlite3_open(name, &db);
-    
-    if(rc) {
+    if(sqlite3_open(name, &db)) {
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
         return 1;
     }
 
-    rc = sqlite3_exec(db, structure, callback, 0, &zErrMsg);
-    
-    if(rc != SQLITE_OK) {
+    const char user_table[] = "CREATE TABLE IF NOT EXIST users("
+        "contact_id integer PRIMARY KEY,"
+	    "first_name text NOT NULL,"
+	    "last_name text NOT NULL,"
+	    "email text NOT NULL UNIQUE,"
+	    "phone text NOT NULL UNIQUE)";
+
+    sqlite3_exec(db, user_table, callback, 0, &zErrMsg);
+
+
+    if(sqlite3_exec(db, structure, callback, 0, &zErrMsg) != SQLITE_OK) {
         fprintf(stderr, "SQL error: %s\n", zErrMsg);
         sqlite3_free(zErrMsg);
     }
