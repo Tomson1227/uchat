98773144a14fa2017ce3d4da6aa6a96dea57bd5c
achaur <achaur@student.ucode.world>
2021-07-26 17:42:17 +0300

    merged request_response with main

--- libs/API/inc/API.h
+++ libs/API/inc/API.h
@@ -1,17 +1,19 @@
 #ifndef API_H
 #define API_H
 
+#include "sqlite3.h"
 #include <stdio.h>
 #include "cJSON.h"
 #include <string.h>
+// #include "../server/inc/uchat_server.h"
 
 typedef enum s_api
 {
     LOGIN,
     SIGNUP
-} t_api;
+}            t_api;
 
-typedef enum s_response_status
+typedef enum s_rs_status
 {
     LOGIN_OK,
     LOGIN_WRONG_USER,
@@ -19,16 +21,14 @@ typedef enum s_response_status
     SIGNUP_OK,
     SIGNUP_USER_EXIST,
     SINGUP_FAIL
-}   t_response_status;
+}            t_rs_status;
 
-void define_rq_type(const char * const string);
-void receive_rs_log_in_server(const char * const string) ;
-void receive_rs_sign_up_server(const char * const string);
 cJSON *receive_rs_log_in_client(const char * const string);
 cJSON *receive_rs_sign_up_client(const char * const string);
 char *send_rq_log_in_client(char *username, char *password);
 char *send_rq_sign_in_client(char *username, char *password);
-char *send_rs_log_in_server(t_response_status response);
-char *send_rs_sign_up_server(t_response_status response);
+
+//Server function
+void process_rq_server(const char *const string, sqlite3 *db);
 
 #endif /* API_H */
--- libs/API/src/API.c
+++ libs/API/src/API.c
@@ -1,213 +1,240 @@
 #include "API.h"
 
 /*--- Static fuctions declarations ---*/
+static void check_error(void);
+static void receive_rq_sign_up_server(const char *const string, sqlite3 *db);
+static void receive_rq_log_in_server(const char *const string, sqlite3 *db);
+static char *send_rs_sign_up_server(t_rs_status response);
+static char *send_rs_log_in_server(t_rs_status response);
 /*-------------------------------------*/
 /*--- Public functions definitions ---*/
 
-void receive_rs_log_in_server(const char * const string) 
+void process_rq_server(const char *const string, sqlite3 *db)
 {
-    cJSON *login = NULL;
-    cJSON *pass = NULL;
-    cJSON *rq_log_in = cJSON_Parse(string);
-    char *login_parsed = NULL;
-    char *pass_parsed = NULL;
+    cJSON *rq = NULL;
 
-    if (rq_log_in == NULL) {
-        const char *error_ptr = cJSON_GetErrorPtr();
-        if (error_ptr != NULL) {
-            fprintf(stderr, "Error before: %s\n", error_ptr);
+    if ((rq = cJSON_Parse(string)))
+    {
+        cJSON *type = cJSON_GetObjectItemCaseSensitive(rq, "type");
+
+        switch ((int)type->valuedouble)
+        {
+        case LOGIN:
+            receive_rq_log_in_server(string, db);
+            break;
+        case SIGNUP:
+            receive_rq_sign_up_server(string, db);
+            break;
+        default:
+            /* UNKNOWN REQUEST */
+            break;
         }
-        cJSON_Delete(rq_log_in);
-    }
-    else {
-        login = cJSON_GetObjectItemCaseSensitive(rq_log_in, "login");
-        pass = cJSON_GetObjectItemCaseSensitive(rq_log_in, "pass");
-        login_parsed = cJSON_Print(login);
-        pass_parsed = cJSON_Print(pass);
 
-        printf("Received login: %s\tReceived password: %s\n", login->valuestring, pass->valuestring);
+        cJSON_Delete(rq);
     }
-
-    cJSON_Delete(rq_log_in);
+    else
+        check_error();
 }
 
-void receive_rs_sign_up_server(const char * const string) {
-    cJSON *login = NULL;
-    cJSON *pass = NULL;
-    char *login_parsed = NULL;
-    char *pass_parsed = NULL;
-    cJSON *rs_sign_up = cJSON_Parse(string);
 
-    if (rs_sign_up == NULL) {
-        const char *error_ptr = cJSON_GetErrorPtr();
-        if (error_ptr != NULL) {
-            fprintf(stderr, "Error before: %s\n", error_ptr);
-        }
-        cJSON_Delete(rs_sign_up);
+cJSON *receive_rs_log_in_client(const char *const string)
+{
+    cJSON *rs_log_in = NULL;
+
+    if ((rs_log_in = cJSON_Parse(string)))
+    {
+        cJSON *status = status = cJSON_GetObjectItemCaseSensitive(rs_log_in, "status");
+        printf("Received status %s\n", status->valuestring);
     }
-    else {
-        login = cJSON_GetObjectItemCaseSensitive(rs_sign_up, "login");
-        pass = cJSON_GetObjectItemCaseSensitive(rs_sign_up, "pass");
-        login_parsed = cJSON_Print(login);
-        pass_parsed = cJSON_Print(pass);
+    else
+        check_error();
 
-        printf("Received login %s\nReceived pass: %s\n", login_parsed, pass_parsed);
-    }
-
-    cJSON_Delete(rs_sign_up);
-}
-
-cJSON *receive_rs_log_in_client(const char * const string) {
-    cJSON *status = NULL;
-    cJSON *rs_log_in = cJSON_Parse(string);
-    char *status_parsed = NULL;
-
-    if (rs_log_in == NULL) {
-        const char *error_ptr = cJSON_GetErrorPtr();
-        if (error_ptr != NULL) {
-            fprintf(stderr, "Error before: %s\n", error_ptr);
-        }
-        cJSON_Delete(rs_log_in);
-    }
-    else {
-        status = cJSON_GetObjectItemCaseSensitive(rs_log_in, "status");
-        status_parsed = cJSON_Print(status);
-        printf("Received status %s\n", status_parsed);
-    }
     return rs_log_in;
 }
 
-cJSON *receive_rs_sign_up_client(const char * const string) {
-    cJSON *status = NULL;
-    cJSON *rs_sign_up = cJSON_Parse(string);
-    char *status_parsed = NULL;
+cJSON *receive_rs_sign_up_client(const char *const string)
+{
+    cJSON *rs_sign_up = NULL;
 
-    if (rs_sign_up == NULL) {
+    if ((rs_sign_up = cJSON_Parse(string)))
+    {
+        cJSON *status = cJSON_GetObjectItemCaseSensitive(rs_sign_up, "status");
+        printf("Received status %s\n", status->valuestring);
+    }
+    else
+    {
         const char *error_ptr = cJSON_GetErrorPtr();
-        if (error_ptr != NULL) {
+
+        if (error_ptr != NULL)
             fprintf(stderr, "Error before: %s\n", error_ptr);
-        }
-        cJSON_Delete(rs_sign_up);
     }
-    else {
-        status = cJSON_GetObjectItemCaseSensitive(rs_sign_up, "status");
-        status_parsed = cJSON_Print(status);
-        printf("Received status %s\n", status_parsed);
-    }
+
     return rs_sign_up;
 }
 
-char *send_rq_log_in_client(char *username, char *password) {
+char *send_rq_log_in_client(char *username, char *password)
+{
     char *string = NULL;
-    cJSON *type = NULL;
-    cJSON *login = NULL;
-    cJSON *pass = NULL;
-    cJSON *request_log_in = cJSON_CreateObject();
+    cJSON *request_log_in = NULL;
 
-    if (request_log_in == NULL) {
-        cJSON_Delete(request_log_in);
-    }
-    else {
-        type = cJSON_CreateString("REQUEST_LOGIN");
-        cJSON_AddItemToObject(request_log_in, "type", type);
-        login = cJSON_CreateString(username);
-        pass = cJSON_CreateString(password);
+    if ((request_log_in = cJSON_CreateObject()))
+    {
+        cJSON *type = cJSON_CreateNumber(LOGIN);
+        cJSON *login = cJSON_CreateString(username);
+        cJSON *pass = cJSON_CreateString(password);
+
         cJSON_AddItemToObject(request_log_in, "type", type);
         cJSON_AddItemToObject(request_log_in, "login", login);
-        cJSON_AddItemToObject(request_log_in, "pass", pass); 
+        cJSON_AddItemToObject(request_log_in, "pass", pass);
+
         string = cJSON_Print(request_log_in);
+        cJSON_Delete(request_log_in);
     }
+
     return string;
 }
 
-char *send_rq_sign_in_client(char *username, char *password) {
+char *send_rq_sign_in_client(char *username, char *password)
+{
     char *string = NULL;
-    cJSON *type = NULL;
-    cJSON *login = NULL;
-    cJSON *pass = NULL;
-    cJSON *request_sign_up = cJSON_CreateObject();
+    cJSON *request_sign_up = NULL;
 
-    if (request_sign_up == NULL) {
-        cJSON_Delete(request_sign_up);
-    }
-    else {
-        type = cJSON_CreateString("REQUEST_SIGNUP");
-        cJSON_AddItemToObject(request_sign_up, "type", type);
-        login = cJSON_CreateString(username);
-        pass = cJSON_CreateString(password);
+    if ((request_sign_up = cJSON_CreateObject()))
+    {
+        cJSON *type = cJSON_CreateNumber(SIGNUP);
+        cJSON *login = cJSON_CreateString(username);
+        cJSON *pass = cJSON_CreateString(password);
+
         cJSON_AddItemToObject(request_sign_up, "type", type);
         cJSON_AddItemToObject(request_sign_up, "login", login);
-        cJSON_AddItemToObject(request_sign_up, "pass", pass); 
+        cJSON_AddItemToObject(request_sign_up, "pass", pass);
+
         string = cJSON_Print(request_sign_up);
+        cJSON_Delete(request_sign_up);
     }
+    else
+        check_error();
+
     return string;
 }
 
-char *send_rs_log_in_server(t_response_status response) {
-    char *string = NULL;
-    cJSON *type = NULL;
-    cJSON *status = NULL;
-    cJSON *rs_log_in = cJSON_CreateObject();
+void process_rs_client(const char *const string)
+{
+    cJSON *rq = NULL;
 
-    if (rs_log_in == NULL) {
-        cJSON_Delete(rs_log_in);
-    }
-    else {
-        type = cJSON_CreateString("RESPONSE_LOGIN");
-        cJSON_AddItemToObject(rs_log_in, "type", type);        
-        
-        status = cJSON_CreateNumber(response);
-        cJSON_AddItemToObject(rs_log_in, "status", status);
-        string = cJSON_Print(rs_log_in);
-    }
-    
-    cJSON_Delete(rs_log_in);
-    return string;
-}
+    if ((rq = cJSON_Parse(string)))
+    {
+        cJSON *type = cJSON_GetObjectItemCaseSensitive(rq, "type");
 
-char *send_rs_sign_up_server(t_response_status response) {
-    char *string = NULL;
-    cJSON *type = NULL;
-    cJSON *status = NULL;
-    cJSON *rs_sign_up = cJSON_CreateObject();
-
-    if (rs_sign_up == NULL) {
-        cJSON_Delete(rs_sign_up);
-    }
-    else {
-        type = cJSON_CreateString("RESPONSE_SIGNUP");
-        cJSON_AddItemToObject(rs_sign_up, "type", type);
-        status = cJSON_CreateNumber(response);
-        cJSON_AddItemToObject(rs_sign_up, "status", status);
-        string = cJSON_Print(rs_sign_up);
-    }
-    return string;
-}
-
-void define_rq_type(const char * const string) {
-    cJSON *type = NULL;
-    cJSON *rq = cJSON_Parse(string);
-    char *type_parsed;
-
-    if (rq == NULL) {
-        const char *error_ptr = cJSON_GetErrorPtr();
-        if (error_ptr != NULL) {
-            fprintf(stderr, "Error before: %s\n", error_ptr);
+        switch ((int)type->valuedouble)
+        {
+        case LOGIN:
+            receive_rs_log_in_client(string);
+            break;
+        case SIGNUP:
+            receive_rs_sign_up_client(string);
+            break;
+        default:
+            /* UNKNOWN RESPONSE */
+            break;
         }
+
         cJSON_Delete(rq);
     }
-    type = cJSON_GetObjectItemCaseSensitive(rq, "type");
-    type_parsed = cJSON_Print(type);
-    if (strcmp(type_parsed, "\"REQUEST_LOGIN\"") == 0)
-        receive_rs_log_in_server(string);
-    if (strcmp(type_parsed, "\"REQUEST_SIGNUP\"") == 0) 
-        receive_rs_sign_up_server(string);
-    if (strcmp(type_parsed, "\"RESPONSE_LOGIN\"") == 0) 
-        receive_rs_log_in_client(string);
-    if (strcmp(type_parsed, "\"RESPONSE_SIGNUP\"") == 0) 
-        receive_rs_sign_up_client(string);
+    else
+        check_error();
 }
 
 /*-------------------------------------*/
 /*--- Static fuctions definitions ---*/
 /*-------------------------------------*/
+
+static void receive_rq_log_in_server(const char *const string, sqlite3 *db)
+{
+    cJSON *rq_log_in = NULL;
+
+    if ((rq_log_in = cJSON_Parse(string)))
+    {
+        cJSON *log_in = cJSON_GetObjectItemCaseSensitive(rq_log_in, "login");
+        cJSON *pass = cJSON_GetObjectItemCaseSensitive(rq_log_in, "pass");
+
+        // t_rs_status status = sign_up(db, log_in->valuestring, pass->valuestring);
+        /* SERVER PROCESS REQUEST */
+        /* SERVER SEND RESPONCE */
+
+        cJSON_Delete(rq_log_in);
+    }
+    else
+        check_error();
+}
+
+static void receive_rq_sign_up_server(const char *const string, sqlite3 *db)
+{
+    cJSON *rs_sign_up = NULL;
+
+    if ((rs_sign_up = cJSON_Parse(string)))
+    {
+        cJSON *log_in = cJSON_GetObjectItemCaseSensitive(rs_sign_up, "login");
+        cJSON *pass = cJSON_GetObjectItemCaseSensitive(rs_sign_up, "pass");
+
+        // t_rs_status status = login(db, log_in->valuestring, pass->valuestring);
+        /* SERVER PROCESS REQUEST */
+        /* SERVER SEND RESPONCE */
+
+        cJSON_Delete(rs_sign_up);
+    }
+    else
+        check_error();
+}
+
+static char *send_rs_log_in_server(t_rs_status response)
+{
+    char *string = NULL;
+    cJSON *rs_log_in = NULL;
+
+    if ((rs_log_in = cJSON_CreateObject()))
+    {
+        cJSON *type = cJSON_CreateNumber(LOGIN);
+        cJSON *status = cJSON_CreateNumber(response);
+
+        cJSON_AddItemToObject(rs_log_in, "type", type);
+        cJSON_AddItemToObject(rs_log_in, "status", status);
+
+        string = cJSON_Print(rs_log_in);
+        cJSON_Delete(rs_log_in);
+    }
+    else
+        check_error();
+
+    return string;
+}
+
+static char *send_rs_sign_up_server(t_rs_status response)
+{
+    char *string = NULL;
+    cJSON *rs_sign_up = NULL;
+
+    if ((rs_sign_up = cJSON_CreateObject()))
+    {
+        cJSON *type = cJSON_CreateNumber(SIGNUP);
+        cJSON *status = cJSON_CreateNumber(response);
+
+        cJSON_AddItemToObject(rs_sign_up, "type", type);
+        cJSON_AddItemToObject(rs_sign_up, "status", status);
+
+        string = cJSON_Print(rs_sign_up);
+        cJSON_Delete(rs_sign_up);
+    }
+    else
+        check_error();
+
+    return string;
+}
+
+static void check_error(void)
+{
+    const char *error_ptr = cJSON_GetErrorPtr();
+
+    if (error_ptr != NULL)
+        fprintf(stderr, "Error before: %s\n", error_ptr);
+}
